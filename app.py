837f69e31306b322d8ead289983303895610d416
khushixxv <khuship1025@gmail.com>
2024-04-24 19:17:59 -0400

    add trend lifecycle and channel growth tabs

--- app.py
+++ app.py
@@ -18,5 +18,13 @@ def scatterplot():
 def map():
     return render_template('map.html')
 
+@app.route('/trendlifecycle')
+def trendlifecycle():
+    return render_template('trendlifecylce.html')
+
+@app.route('/channelgrowth')
+def channelgrowth():
+    return render_template('channelgrowth.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
