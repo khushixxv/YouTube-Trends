efd00e9a01b9e9affeb3d4f0256301bf3942f0dc
cynthiachennn <94323798+cynthiachennn@users.noreply.github.com>
2024-04-27 08:06:51 -0400

    added lifecycle comments + animation

---
+++ lifecycle_views.py
@@ -0,0 +1,115 @@
+import plotly.express as px
+import plotly.graph_objects as go
+import pandas as pd
+import json
+
+# Map abbreviations to full country names
+country_names = {
+    'BR': 'Brazil',
+    'CA': 'Canada',
+    'DE': 'Germany',
+    'FR': 'France',
+    'GB': 'Great Britain',
+    'IN': 'India',
+    'JP': 'Japan',
+    'KR': 'South Korea',
+    'MX': 'Mexico',
+    'RU': 'Russia',
+    'US': 'United States'
+}
+
+dfs= []
+for country in ['BR', 'CA', 'DE', 'FR', 'GB', 'IN', 'JP', 'KR', 'MX', 'RU', 'US']:
+    df = pd.read_csv(f'../archive/{country}_youtube_trending_data.csv')
+    df = df[["video_id", "title", "view_count", "trending_date"]]
+    df["country"] = country_names[country]
+    dfs.append(df)
+
+df_combined = pd.concat(dfs)
+
+# Custom color map for countries
+color_map = {
+    'Brazil': 'blue',
+    'Canada': 'red',
+    'Germany': 'green',
+    'France': 'yellow',
+    'Great Britain': 'purple',
+    'India': 'orange',
+    'Japan': 'pink',
+    'South Korea': 'lightgreen',
+    'Mexico': 'teal',
+    'Russia': 'gray',
+    'United States': 'darkblue'
+}
+
+# Concatenate all the DataFrames
+# get year/month
+df_combined['trending_date'] = pd.to_datetime(df_combined['trending_date'])
+df_combined = df_combined[df_combined['trending_date'].dt.year == 2023]
+# df_combined['trending_date'] = df_combined['trending_date'].dt.month
+
+df_combined = df_combined.groupby(['video_id', 'trending_date']).agg({
+    'country': lambda x: list(set(x)),
+    'view_count': 'mean',
+    'title': 'first',
+}).head(20000).reset_index()
+
+print(df_combined[50:75])
+
+df_combined = df_combined.groupby('video_id').apply(lambda x: x.sort_values('trending_date')).reset_index(drop=True)
+# df_combined['days_past_first'] = df_combined['trending_date'].transform(lambda x: (x - x.min()).dt.days)
+# df_combined['view_count_change'] = df_combined['view_count'].diff()
+# df_combined['view_count_change'] = df_combined['view_count_change'].fillna(0)
+# print(df_combined[["video_id", "days_past_first", "view_count_change"]])
+
+# df_processed = df_combined[["video_id", "title", "view_count", "trending_date", "country"]].reset_index(drop=True)
+# print(df_combined.groupby('video_id').get_group(df_combined.groupby('video_id').groups.keys()))
+# print(df_combined[50:75])
+grouped = df_combined.groupby('video_id')#.drop('video_id', axis=1)
+# print(df_combined[50:75])
+# print('abt to sort and...')
+count = 0
+fig = go.Figure()
+for name, group in grouped:
+    count+=1
+    # if count == 1000:
+    #     break
+    # print(group[['title', 'days_past_first', 'view_count_change']])
+    # group = group.apply(lambda x: x.sort_values('trending_date')) # .reset_index(drop=True)
+    # group = group.sort_values('trending_date')
+    # print(group)
+    for row in group:
+        group['days_past_first'] = group['trending_date'].transform(lambda x: (x - x.min()).dt.days)
+        group['view_count_change'] = group['view_count'].diff()
+    
+    # print(group['title'])
+
+    fig.add_trace(go.Scatter(
+        x=group['days_past_first'], 
+        y=group['view_count_change'], 
+        mode='lines', 
+        line=dict(color=color_map[group['country'].iloc[0][0]]), 
+        name=group['title'].iloc[0][0:25] + "...",
+        hovertemplate =
+            '<i>Days past first</i>: %{x}<br>'+
+            '<b>View count change</b>: %{y}<br>',
+            # '<b>Video ID</b>: %{name} <br>' +
+            # '<b>Countries Trending</b>: %{text}<br>',
+        text=group['country'].iloc[0]
+        ))
+        
+    fig.update_layout(
+        title="Video Life Cycle Analysis - VIEWS",
+        xaxis_title="Day Trending",
+        yaxis_title="Rate of Change in Views",
+        legend_title="Video Title",
+    )
+
+# fig = px.line(df_combined, x='days_past_first', y='view_count_change', color='video_id')
+# fig.update_layout(title='View Count Change Over Time by Video ID')
+# fig.update_xaxes(title='Days Trending')
+# fig.update_yaxes(title='View Count Change')
+
+fig.show()
+
+fig.write_html('static/lifecycle_views.html')
--- scatterplot_animated.py
+++ scatterplot_animated.py
@@ -1,66 +1,32 @@
 import plotly.express as px
 import pandas as pd
 
+# Map abbreviations to full country names
+country_names = {
+    'BR': 'Brazil',
+    'CA': 'Canada',
+    'DE': 'Germany',
+    'FR': 'France',
+    'GB': 'Great Britain',
+    'IN': 'India',
+    'JP': 'Japan',
+    'KR': 'South Korea',
+    'MX': 'Mexico',
+    'RU': 'Russia',
+    'US': 'United States'
+}
 
-# Load the datasets for each country
-df_br = pd.read_csv('../archive/BR_youtube_trending_data.csv')  # Brazil
-df_ca = pd.read_csv('../archive/CA_youtube_trending_data.csv')  # Canada
-df_de = pd.read_csv('../archive/DE_youtube_trending_data.csv')  # Germany
-df_fr = pd.read_csv('../archive/FR_youtube_trending_data.csv')  # France
-df_gb = pd.read_csv('../archive/GB_youtube_trending_data.csv')  # Great Britain
-df_in = pd.read_csv('../archive/IN_youtube_trending_data.csv')  # India
-df_jp = pd.read_csv('../archive/JP_youtube_trending_data.csv')  # Japan
-df_kr = pd.read_csv('../archive/KR_youtube_trending_data.csv')  # South Korea
-df_mx = pd.read_csv('../archive/MX_youtube_trending_data.csv')  # Mexico
-df_ru = pd.read_csv('../archive/RU_youtube_trending_data.csv')  # Russia
-df_us = pd.read_csv('../archive/US_youtube_trending_data.csv')  # United States
-
-#only take data from december 2023
-# Explicitly convert the 'trending_date' column to datetime
-# df_br['trending_date'] = f'{pd.to_datetime(df_br['trending_date']).dt.year}-{pd.to_datetime(df_br['trending_date']).dt.month}'
-# df_ca['trending_date'] = f'{pd.to_datetime(df_ca['trending_date']).dt.year}-{pd.to_datetime(df_br['trending_date']).dt.month}'
-# df_de['trending_date'] = pd.to_datetime(df_de['trending_date']).dt.date
-# df_fr['trending_date'] = pd.to_datetime(df_fr['trending_date']).dt.date
-# df_gb['trending_date'] = pd.to_datetime(df_gb['trending_date']).dt.date
-# df_in['trending_date'] = pd.to_datetime(df_in['trending_date']).dt.date
-# df_jp['trending_date'] = pd.to_datetime(df_jp['trending_date']).dt.date
-# df_kr['trending_date'] = pd.to_datetime(df_kr['trending_date']).dt.date
-# df_mx['trending_date'] = pd.to_datetime(df_mx['trending_date']).dt.date
-# df_ru['trending_date'] = pd.to_datetime(df_ru['trending_date']).dt.date
-# df_us['trending_date'] = pd.to_datetime(df_us['trending_date']).dt.date
-
-# selected_month = 12
-# # Filter for all of December 2023
-# df_br = df_br[(df_br['trending_date'].dt.year == 2023) & (df_br['trending_date'].dt.month == selected_month)]
-# df_ca = df_ca[(df_ca['trending_date'].dt.year == 2023) & (df_ca['trending_date'].dt.month == selected_month)]
-# df_de = df_de[(df_de['trending_date'].dt.year == 2023) & (df_de['trending_date'].dt.month == selected_month)]
-# df_fr = df_fr[(df_fr['trending_date'].dt.year == 2023) & (df_fr['trending_date'].dt.month == selected_month)]
-# df_gb = df_gb[(df_gb['trending_date'].dt.year == 2023) & (df_gb['trending_date'].dt.month == selected_month)]
-# df_in = df_in[(df_in['trending_date'].dt.year == 2023) & (df_in['trending_date'].dt.month == selected_month)]
-# df_jp = df_jp[(df_jp['trending_date'].dt.year == 2023) & (df_jp['trending_date'].dt.month == selected_month)]
-# df_kr = df_kr[(df_kr['trending_date'].dt.year == 2023) & (df_kr['trending_date'].dt.month == selected_month)]
-# df_mx = df_mx[(df_mx['trending_date'].dt.year == 2023) & (df_mx['trending_date'].dt.month == selected_month)]
-# df_ru = df_ru[(df_ru['trending_date'].dt.year == 2023) & (df_ru['trending_date'].dt.month == selected_month)]
-# df_us = df_us[(df_us['trending_date'].dt.year == 2023) & (df_us['trending_date'].dt.month == selected_month)]
-
-# Add a column to distinguish the countries
-df_br['Country'] = 'Brazil'
-df_ca['Country'] = 'Canada'
-df_de['Country'] = 'Germany'
-df_fr['Country'] = 'France'
-df_gb['Country'] = 'Great Britain'
-df_in['Country'] = 'India'
-df_jp['Country'] = 'Japan'
-df_kr['Country'] = 'South Korea'
-df_mx['Country'] = 'Mexico'
-df_ru['Country'] = 'Russia'
-df_us['Country'] = 'United States'
-# Concatenate all the DataFrames
-df_combined = pd.concat([df_br, df_ca, df_de, df_fr, df_gb, df_in, df_jp, df_kr, df_mx, df_ru, df_us])
-# get year/month
-df_combined['trending_date'] = pd.to_datetime(df_combined['trending_date'])
-df_combined = df_combined[df_combined['trending_date'].dt.year == 2023]
-df_combined['trending_date'] = df_combined['trending_date'].dt.month
+dfs= []
+for country in ['BR', 'CA', 'DE', 'FR', 'GB', 'IN', 'JP', 'KR', 'MX', 'RU', 'US']:
+    df = pd.read_csv(f'../archive/{country}_youtube_trending_data.csv')
+    df = df[["video_id", "title", "view_count", "likes", "comment_count", "trending_date"]]
+    df["country"] = country_names[country]
+    df['trending_date'] = pd.to_datetime(df['trending_date'])
+    df = df[df['trending_date'].dt.year == 2023]
+    df = df.groupby('trending_date').head(75).reset_index()
+    dfs.append(df)
+df_combined = pd.concat(dfs)
+df_combined['anim_frame'] = df_combined['trending_date'].dt.dayofyear
 
 # Custom color map for countries
 color_map = {
@@ -80,16 +46,15 @@ color_map = {
 import plotly.express as px
 import pandas as pd
 
-# Assuming df_combined and color_map are defined elsewhere in your code
 
-fig = px.scatter(df_combined, x='view_count', y='likes', color='Country', size='comment_count',
+fig = px.scatter(df_combined, x='view_count', y='likes', color='country', size='comment_count',
                 title='2023 Trending Videos: View Count vs Likes by Country',
-                labels={'view_count': 'View Count (Millions)', 'likes': 'Likes (Millions)', 'comment_count': 'Comment Count'},
+                labels={'title': 'Title', 'view_count': 'View Count (Millions)', 'likes': 'Likes (Millions)', 'comment_count': 'Comment Count'},
                 opacity=0.6,  # Adjust opacity here
                 color_discrete_map=color_map, 
                 log_x=True,
                 log_y=True,
-                animation_frame='trending_date')
+                animation_frame='anim_frame')
 
 # Remove the white border from each node
 fig.update_traces(marker=dict(line=dict(width=0)))
